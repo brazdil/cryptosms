a193b7daf657761223146a479a8a005895239980
db538 <db538@f70da828-fea9-4f00-a764-63cabeb938f8>
2011-07-12 12:11:32 +0000

    Progress dialog when sendind SMS, handling errors

--- trunk/src/uk/ac/cam/db538/securesms/SMSReceiver.java
+++ trunk/src/uk/ac/cam/db538/securesms/SMSReceiver.java
@@ -3,6 +3,7 @@ package uk.ac.cam.db538.securesms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
@@ -17,17 +18,27 @@ public class SMSReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		Resources r = context.getResources();
+		
 		if (intent.getAction().equals(SMS_RECEIVED)) {
-			Bundle bundle = intent.getExtras();
-			if (bundle != null) {
-				Object[] pdus = (Object[]) bundle.get("pdus");
-				SmsMessage[] messages = new SmsMessage[pdus.length];
-				for (int i = 0; i < pdus.length; ++i)
-					messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-				for (SmsMessage msg : messages) {
-					byte[] data = msg.getUserData();
-					String body = new String(data);
-					Toast.makeText(context, msg.getOriginatingAddress() + ": " + body, Toast.LENGTH_LONG).show();
+			// check the port number
+			String[] uri = intent.getDataString().split(":");
+			int port = Integer.parseInt(uri[uri.length - 1]);
+			if (port == r.getInteger(R.integer.presets_data_sms_port))
+			{
+				// for each message
+				Bundle bundle = intent.getExtras();
+				if (bundle != null) {
+					Object[] pdus = (Object[]) bundle.get("pdus");
+					SmsMessage[] messages = new SmsMessage[pdus.length];
+					for (int i = 0; i < pdus.length; ++i)
+						messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+					for (SmsMessage msg : messages) {
+						// get the data in the message
+						byte[] data = msg.getUserData();
+						String body = new String(data);
+						Toast.makeText(context, msg.getOriginatingAddress() + ": " + body, Toast.LENGTH_LONG).show();
+					}
 				}
 			}
 		}
