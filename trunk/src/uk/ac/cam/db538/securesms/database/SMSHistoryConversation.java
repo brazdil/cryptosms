ab0ddc0869e4871e9abc668b9b8c44f8465a0108
db538 <db538@f70da828-fea9-4f00-a764-63cabeb938f8>
2011-07-14 16:06:04 +0000

    Flags added to conversations in the history file

--- trunk/src/uk/ac/cam/db538/securesms/database/SMSHistoryConversation.java
+++ trunk/src/uk/ac/cam/db538/securesms/database/SMSHistoryConversation.java
@@ -12,11 +12,13 @@ public class SMSHistoryConversation {
 	
 	private static final String CHARSET_LATIN = "ISO-8859-1";
 	
+	private static final int LENGTH_FLAGS = 1;
 	private static final int LENGTH_PHONENUMBER = 32;
 	private static final int LENGTH_TIMESTAMP = 29;
 	private static final int LENGTH_SESSIONKEY = 32;
 
-	private static final int OFFSET_PHONENUMBER = 0;
+	private static final int OFFSET_FLAGS = 0;
+	private static final int OFFSET_PHONENUMBER = OFFSET_FLAGS + LENGTH_FLAGS;
 	private static final int OFFSET_TIMESTAMP = OFFSET_PHONENUMBER + LENGTH_PHONENUMBER;
 	private static final int OFFSET_SESSIONKEY_OUTGOING = OFFSET_TIMESTAMP + LENGTH_TIMESTAMP;
 	private static final int OFFSET_SESSIONKEY_INCOMING = OFFSET_SESSIONKEY_OUTGOING + LENGTH_SESSIONKEY;
@@ -29,6 +31,7 @@ public class SMSHistoryConversation {
 	
 	private static final int LENGTH_RANDOMDATA = OFFSET_RANDOMDATA - OFFSET_MSGSINDEX;	
 
+	private boolean mKeysExchanged;
 	private String mPhoneNumber;
 	private Time mTimeStamp;
 	private byte[] mSessionKey_Out;
@@ -37,7 +40,8 @@ public class SMSHistoryConversation {
 	private long mIndexPrev;
 	private long mIndexNext;
 	
-	SMSHistoryConversation(String phoneNumber, Time timeStamp, byte[] sessionKey_Out, byte[] sessionKey_In, long indexMessages, long indexPrev, long indexNext) {
+	SMSHistoryConversation(boolean keysExchanged, String phoneNumber, Time timeStamp, byte[] sessionKey_Out, byte[] sessionKey_In, long indexMessages, long indexPrev, long indexNext) {
+		mKeysExchanged = keysExchanged;
 		mPhoneNumber = phoneNumber;
 		mTimeStamp = timeStamp;
 		mSessionKey_Out = sessionKey_Out;
@@ -47,6 +51,14 @@ public class SMSHistoryConversation {
 		mIndexNext = indexNext;
 	}
 	
+	public boolean getKeysExchanged() {
+		return mKeysExchanged;
+	}
+
+	public void setKeysExchanged(boolean keysExchanged) {
+		mKeysExchanged = keysExchanged;
+	}
+
 	public String getPhoneNumber() {
 		return mPhoneNumber;
 	}
@@ -109,6 +121,12 @@ public class SMSHistoryConversation {
 			
 			ByteBuffer convBuffer = ByteBuffer.allocate(SMSHistory.ENCRYPTED_ENTRY_SIZE);
 			
+			// flags
+			byte flags = 0;
+			if (conversation.mKeysExchanged)
+				flags |= (byte) ((1 << 7) & 0xFF);
+			convBuffer.put(flags);
+			
 			// phone number
 			temp = conversation.mPhoneNumber.getBytes(CHARSET_LATIN);
 			byte[] bytePhoneNumber = new byte[LENGTH_PHONENUMBER];
@@ -159,6 +177,8 @@ public class SMSHistoryConversation {
 		try {
 			byte[] dataPlain = Encryption.decodeWithPassphrase(dataEncrypted);
 			
+			byte flags = dataPlain[OFFSET_FLAGS];
+			boolean keysExchanged = ((flags & (1 << 7)) == 0) ? false : true;
 			byte[] dataPhoneNumber = new byte[LENGTH_PHONENUMBER];
 			System.arraycopy(dataPlain, OFFSET_PHONENUMBER, dataPhoneNumber, 0, LENGTH_PHONENUMBER);
 			byte[] dataTimeStamp = new byte[LENGTH_TIMESTAMP];
@@ -170,7 +190,8 @@ public class SMSHistoryConversation {
 			Time timeStamp = new Time();
 			timeStamp.parse3339(new String(dataTimeStamp, CHARSET_LATIN));
 	
-			return new SMSHistoryConversation(new String(dataPhoneNumber, CHARSET_LATIN), 
+			return new SMSHistoryConversation(keysExchanged,
+			                                  new String(dataPhoneNumber, CHARSET_LATIN), 
 			                                  timeStamp, 
 			                                  dataSessionKey_Out, 
 			                                  dataSessionKey_In, 
