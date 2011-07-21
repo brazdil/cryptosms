f0f4c167ba1fb36efe02f43d326f35facf49c429
db538 <db538@f70da828-fea9-4f00-a764-63cabeb938f8>
2011-07-21 10:38:29 +0000

    Conversation tests

--- test/src/uk/ac/cam/db538/securesms/database/Conversation_Test.java
+++ test/src/uk/ac/cam/db538/securesms/database/Conversation_Test.java
@@ -4,171 +4,221 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import uk.ac.cam.db538.securesms.encryption.Encryption;
+
 import android.text.format.Time;
 
 import junit.framework.TestCase;
 
 public class Conversation_Test extends TestCase {
 
+	// Testing data
+	private String phoneNumber = "+447896512369";
+	private String phoneNumberLong = "+1234567890126549873sdfsat6ewrt987wet3df1g3s2g1e6r5t46wert4dfsgdfsg";
+	private String phoneNumberResult = "+1234567890126549873sdfsat6ewrt9";
+	private Time timeStamp = new Time();
+	private long indexSessionKeys = 122L;
+	private long indexMessages = 120L;
+	private long indexPrev = 12L;
+	private long indexNext = 15L;
+
+	private void setData(Conversation conv, boolean longPhoneNumber) {
+		conv.setPhoneNumber((longPhoneNumber) ? phoneNumberLong : phoneNumber);
+		conv.setTimeStamp(timeStamp);
+		conv.setIndexSessionKeys(indexSessionKeys);
+		conv.setIndexMessages(indexMessages);
+		conv.setIndexPrev(indexPrev);
+		conv.setIndexNext(indexNext);
+	}
+	
+	private void checkData(Conversation conv, boolean longPhoneNumber) {
+		assertEquals(conv.getPhoneNumber(), (longPhoneNumber) ? phoneNumberResult : phoneNumber);
+		assertEquals(Time.compare(timeStamp, conv.getTimeStamp()), 0);
+		assertEquals(conv.getIndexSessionKeys(), indexSessionKeys);
+		assertEquals(conv.getIndexMessages(), indexMessages);
+		assertEquals(conv.getIndexPrev(), indexPrev);
+		assertEquals(conv.getIndexNext(), indexNext);
+	}
+
 	protected void setUp() throws Exception {
 		super.setUp();
 		Common.clearFile();
+		
+		timeStamp.set(12, 24, 5, 8, 2, 1985);
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-
-/*	public void testConversation() {
-		Conversation_Old conv;
-		FileEntryConversation entry = new FileEntryConversation("", null, 12L, 15L, 5L, 7L);
+	
+	public void testConversation() throws DatabaseFileException, IOException {
 		// ASSIGNMENT
+		Conversation conv = Conversation.createConversation();
+		setData(conv, false);
+		conv.saveToFile();
+		long index = conv.getEntryIndex();
 		
-		try {
-			conv = new Conversation_Old(120L, entry);
-			assertEquals(conv.getEntryIndex(), 120L);
-			assertEquals(conv.getEntry(), entry);
-		} catch (IndexOutOfBoundsException ex) {
-			assertTrue(false);
-		}
-		
+		// force it to be re-read from file
+		Conversation.forceClearCache();
+		conv = Conversation.getConversation(index);
+		checkData(conv, false);
+	}
+	
+	public void testIndices() throws DatabaseFileException, IOException {
 		// INDEX OUT OF BOUNDS
+		Conversation conv = Conversation.createConversation();
 		
+		// indexSessionKeys
 		try {
-			conv = new Conversation_Old(0x100000000L, entry);
+			conv.setIndexSessionKeys(0x100000000L);
 			assertTrue(false);
 		} catch (IndexOutOfBoundsException ex) {
 		}
 
 		try {
-			conv = new Conversation_Old(0L, entry);
+			conv.setIndexSessionKeys(0L);
+		} catch (IndexOutOfBoundsException ex) {
+			assertTrue(false);
+		}
+
+		try {
+			conv.setIndexSessionKeys(-1L);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException ex) {
+		}
+
+		// indexMessages
+		try {
+			conv.setIndexMessages(0x100000000L);
 			assertTrue(false);
 		} catch (IndexOutOfBoundsException ex) {
 		}
 
 		try {
-			conv = new Conversation_Old(-1L, entry);
+			conv.setIndexMessages(0L);
+		} catch (IndexOutOfBoundsException ex) {
+			assertTrue(false);
+		}
+
+		try {
+			conv.setIndexMessages(-1L);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException ex) {
+		}
+
+		// indexPrev
+		try {
+			conv.setIndexPrev(0x100000000L);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException ex) {
+		}
+
+		try {
+			conv.setIndexPrev(0L);
+		} catch (IndexOutOfBoundsException ex) {
+			assertTrue(false);
+		}
+
+		try {
+			conv.setIndexPrev(-1L);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException ex) {
+		}
+
+		// indexNext
+		try {
+			conv.setIndexNext(0x100000000L);
+			assertTrue(false);
+		} catch (IndexOutOfBoundsException ex) {
+		}
+
+		try {
+			conv.setIndexNext(0L);
+		} catch (IndexOutOfBoundsException ex) {
+			assertTrue(false);
+		}
+
+		try {
+			conv.setIndexNext(-1L);
 			assertTrue(false);
 		} catch (IndexOutOfBoundsException ex) {
 		}
 	}
 
-	public void testSaveUpdate() throws DatabaseFileException, IOException {
-		Database_Old database = Database_Old.getSingleton();
+	public void testCreateData() throws DatabaseFileException, IOException {
+		byte flags = 0;
+
+		Conversation conv = Conversation.createConversation() ;
+		setData(conv, true);
+		conv.saveToFile();
 		
-		String phoneNumber = "+123456789012";
-		Time timeStamp = new Time(); timeStamp.set(5, 2, 1928);
+		// get the generated data
+		byte[] dataEncrypted = Database.getDatabase().getEntry(conv.getEntryIndex()); 
 		
-		Conversation_Old convWrite = database.createConversation("", new Time());
-		Conversation_Old convRead = new Conversation_Old(convWrite.getEntryIndex());
+		// chunk length
+		assertEquals(dataEncrypted.length, Database.CHUNK_SIZE);
 		
-		convWrite.setPhoneNumber(phoneNumber);
-		convWrite.setTimeStamp(timeStamp);
-		convWrite.save();
+		// decrypt the encoded part
+		byte[] dataPlain = Encryption.decryptSymmetric(dataEncrypted, Encryption.retreiveEncryptionKey());
 		
-		convRead.update();
-		assertEquals(convRead.getPhoneNumber(), phoneNumber);
-		assertEquals(Time.compare(convRead.getTimeStamp(), timeStamp), 0);
+		// check the data
+		assertEquals(flags, dataPlain[0]);
+		assertEquals(phoneNumberResult, Database.fromLatin(dataPlain, 1, 32));
+		Time time = new Time(); time.parse3339(Database.fromLatin(dataPlain, 33, 29));
+		assertEquals(Time.compare(time, timeStamp), 0);
+		assertEquals(Database.getInt(dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 16), indexSessionKeys);
+		assertEquals(Database.getInt(dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 12), indexMessages);
+		assertEquals(Database.getInt(dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 8), indexPrev);
+		assertEquals(Database.getInt(dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 4), indexNext);
 	}
 
+	public void testParseData() throws DatabaseFileException, IOException {
+		byte flags = 0;
+		
+		Conversation conv = Conversation.createConversation();
+		long index = conv.getEntryIndex();
+
+		// create plain data
+		byte[] dataPlain = new byte[Database.ENCRYPTED_ENTRY_SIZE];
+		dataPlain[0] = flags;
+		System.arraycopy(Database.toLatin(phoneNumber, 32), 0, dataPlain, 1, 32);
+		System.arraycopy(Database.toLatin(timeStamp.format3339(false), 29), 0, dataPlain, 33, 29);
+		System.arraycopy(Database.getBytes(indexSessionKeys), 0, dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 16, 4);
+		System.arraycopy(Database.getBytes(indexMessages), 0, dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 12, 4);
+		System.arraycopy(Database.getBytes(indexPrev), 0, dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 8, 4);
+		System.arraycopy(Database.getBytes(indexNext), 0, dataPlain, Database.ENCRYPTED_ENTRY_SIZE - 4, 4);
+		
+		// encrypt it and inject it into the file
+		byte[] dataEncrypted = Encryption.encryptSymmetric(dataPlain, Encryption.retreiveEncryptionKey());
+		Database.getDatabase().setEntry(index, dataEncrypted);
+
+		// have it parsed
+		Conversation.forceClearCache();
+		conv = Conversation.getConversation(index);
+		
+		// check the indices
+		checkData(conv, false);
+	}
+	
 	public void testCreateConversation() {
-		Database history;
 		try {
-			history = Database.getSingleton();
-			Time time = new Time(); time.setToNow();
-
 			// check that it takes only one entry
-			int countEmpty = history.getEmptyEntriesCount();
+			int countEmpty = Empty.getEmptyEntriesCount();
 			for (int i = 0; i < Database.ALIGN_SIZE / Database.CHUNK_SIZE * 5; ++i)
 			{
-				history.createConversation("Shut the fuck up!", time);
+				Conversation.createConversation();
 				if (countEmpty == 0)
-					assertEquals(Database.ALIGN_SIZE / Database.CHUNK_SIZE - 1, (countEmpty = history.getEmptyEntriesCount()));
+					assertEquals(Database.ALIGN_SIZE / Database.CHUNK_SIZE - 1, (countEmpty = Empty.getEmptyEntriesCount()));
 				else
-					assertEquals(countEmpty - 1, (countEmpty = history.getEmptyEntriesCount()));
+					assertEquals(countEmpty - 1, (countEmpty = Empty.getEmptyEntriesCount()));
 			}
 			
 			// check structure
-			assertTrue(history.checkStructure());
+			assertTrue(Common.checkStructure());
 		} catch (DatabaseFileException e) {
 			assertTrue(e.getMessage(), false);
 		} catch (IOException e) {
 			assertTrue(e.getMessage(), false);
 		}
 	}
-
-	public void testGetConversation() {
-		Database history;
-		try {
-			history = Database.getSingleton();
-			Time time = new Time(); time.setToNow();
-
-			String phoneNumberUK = "07572458912";
-			String phoneNumberInternational = "+447572458912";
-			String phoneNumberDifferent = "458912";
-			
-			// conv1 should have the UK number stored
-			Conversation conv1 = history.createConversation(phoneNumberUK);
-			assertEquals(conv1.getPhoneNumber(), phoneNumberUK);
-			
-			// try to find it using the international number
-			Conversation conv2 = history.getConversation(phoneNumberInternational);
-			assertEquals(conv1.getEntryIndex(), conv2.getEntryIndex());
-			
-			// it should change the number to international
-			assertEquals(conv2.getPhoneNumber(), phoneNumberInternational);
-			conv1.update();
-			assertEquals(conv1.getPhoneNumber(), phoneNumberInternational);
-			
-			// try finding a different number
-			assertEquals(history.getConversation(phoneNumberDifferent), null);
-			
-			// check structure
-			assertTrue(history.checkStructure());
-		} catch (DatabaseFileException e) {
-			assertTrue(e.getMessage(), false);
-		} catch (IOException e) {
-			assertTrue(e.getMessage(), false);
-		}
-	}
-
-	public void testGetAllConversations() {
-		Database history;
-		try {
-			history = Database.getSingleton();
-			Time time1 = new Time(); time1.set(1, 1, 1998);
-			Time time2 = new Time(); time2.set(1, 1, 1994);
-			Time time3 = new Time(); time3.set(1, 1, 1996);
-			Time time4 = new Time(); time4.set(1, 1, 1989);
-
-			Conversation conv1 = history.createConversation("1");
-			conv1.setTimeStamp(time1);
-			conv1.save();
-			Conversation conv2 = history.createConversation("2");
-			conv2.setTimeStamp(time2);
-			conv2.save();
-			Conversation conv3 = history.createConversation("3");
-			conv3.setTimeStamp(time3);
-			conv3.save();
-			Conversation conv4 = history.createConversation("4");
-			conv4.setTimeStamp(time4);
-			conv4.save();
-			
-			// get all conversations
-			ArrayList<Conversation> list = history.getListOfConversations();
-			
-			// check that it is there and sorted
-			assertEquals(list.get(0).getPhoneNumber(), "4");
-			assertEquals(list.get(1).getPhoneNumber(), "2");
-			assertEquals(list.get(2).getPhoneNumber(), "3");
-			assertEquals(list.get(3).getPhoneNumber(), "1");
-			
-			// check structure
-			assertTrue(history.checkStructure());
-		} catch (DatabaseFileException e) {
-			assertTrue(e.getMessage(), false);
-		} catch (IOException e) {
-			assertTrue(e.getMessage(), false);
-		}
-	}*/
 }
