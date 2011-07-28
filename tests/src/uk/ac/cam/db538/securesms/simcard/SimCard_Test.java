bb9f35399cc891ac86349efc1dbcb55a0c8affdb
db538 <db538@f70da828-fea9-4f00-a764-63cabeb938f8>
2011-07-28 16:12:31 +0000

    AndroidMock works

---
+++ tests/src/uk/ac/cam/db538/securesms/simcard/SimCard_Test.java
@@ -0,0 +1,14 @@
+package uk.ac.cam.db538.securesms.simcard;
+
+import com.google.android.testing.mocking.AndroidMock;
+import com.google.android.testing.mocking.UsesMocks;
+
+import android.test.InstrumentationTestCase;
+
+public class SimCard_Test extends InstrumentationTestCase {
+	
+	@UsesMocks(Object.class)
+	public void testGetAllSimNumbersStored() {
+		Object obj = AndroidMock.createMock(Object.class);
+	}
+}
