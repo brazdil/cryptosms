a25427e4e3295486e171173333818ef221635382
db538 <db538@f70da828-fea9-4f00-a764-63cabeb938f8>
2011-08-01 22:28:27 +0000

    Charset supports UTF-8 and 7-bit ASCII

--- trunk/src/uk/ac/cam/db538/securesms/Charset.java
+++ trunk/src/uk/ac/cam/db538/securesms/Charset.java
@@ -8,6 +8,7 @@ import uk.ac.cam.db538.securesms.encryption.Encryption;
 public class Charset {
 	private static final String CHARSET_ASCII = "US-ASCII";
 	private static final String CHARSET_LATIN = "ISO-8859-1";
+	private static final String CHARSET_UTF8 = "UTF-8";
 
 	public static boolean isConvertableToAscii(String text) {
 		for (int i = 0; i < text.length(); ++i)
@@ -16,23 +17,21 @@ public class Charset {
 		return true;
 	}
 	
-	public static byte[] toAscii(String text) {
-		return null; 
-	}
-
 	/**
-	 * Turns a string into an ASCII series of bytes. 
+	 * Turns a string into a series of bytes. 
 	 * @param text				Encoded string
 	 * @param bufferLength		Maximum size of the resulting array
+	 * @param charset			Resulting charset
 	 * @return
 	 */
-	public static byte[] toLatin(String text, int bufferLength) {
+	private static byte[] toBytes(String text, int bufferLength, String charset) {
 		ByteBuffer buffer = ByteBuffer.allocate(bufferLength);
 
 		byte[] latin = null;
 		try {
-			latin = text.getBytes(CHARSET_LATIN);
+			latin = text.getBytes(charset);
 		} catch (UnsupportedEncodingException e) {
+			return null;
 		}
 		
 		if (latin.length < bufferLength) {
@@ -47,7 +46,36 @@ public class Charset {
 	}
 	
 	/**
-	 * Takes a byte array with ASCII characters in it, and turns it into a string
+	 * Takes a byte array with characters in it, and turns it into a string
+	 * @param bytesData		Data to be processed
+	 * @param offset		Offset in the array
+	 * @param len			Length of data
+	 * @return
+	 */
+	private static String fromBytes(byte[] bytesData, int offset, int len, String charset) {
+		int length = 0;
+		while (length < len && bytesData[offset + length] != 0)
+			++length;
+		
+		try {
+			return new String(bytesData, offset, Math.min(len, length), CHARSET_ASCII);
+		} catch (UnsupportedEncodingException ex) {
+			return null;
+		}		
+	}
+
+	/**
+	 * Turns a string into an 8-bit ASCII series of bytes. 
+	 * @param text				Encoded string
+	 * @param bufferLength		Maximum size of the resulting array
+	 * @return
+	 */
+	public static byte[] toLatin(String text, int bufferLength) {
+		return toBytes(text, bufferLength, CHARSET_LATIN);
+	}
+	
+	/**
+	 * Takes a byte array with 8-bit ASCII characters in it, and turns it into a string
 	 * @param latinData		Data to be processed
 	 * @return
 	 */
@@ -56,22 +84,74 @@ public class Charset {
 	}
 	
 	/**
-	 * Takes a byte array with ASCII characters in it, and turns it into a string
+	 * Takes a byte array with 8-bit ASCII characters in it, and turns it into a string
 	 * @param latinData		Data to be processed
 	 * @param offset		Offset in the array
 	 * @param len			Length of data
 	 * @return
 	 */
 	public static String fromLatin(byte[] latinData, int offset, int len) {
-		int length = 0;
-		while (length < len && latinData[offset + length] != 0)
-			++length;
-		
-		try {
-			return new String(latinData, offset, Math.min(len, length), CHARSET_LATIN);
-		} catch (UnsupportedEncodingException ex) {
-			return null;
-		}		
+		return fromBytes(latinData, offset, len, CHARSET_LATIN);
+	}
+
+	/**
+	 * Turns a string into an 7-bit ASCII series of bytes. 
+	 * @param text				Encoded string
+	 * @param bufferLength		Maximum size of the resulting array
+	 * @return
+	 */
+	public static byte[] toAscii(String text, int bufferLength) {
+		return toBytes(text, bufferLength, CHARSET_ASCII);
+	}
+	
+	/**
+	 * Takes a byte array with 7-bit ASCII characters in it, and turns it into a string
+	 * @param asciiData		Data to be processed
+	 * @return
+	 */
+	public static String fromAscii(byte[] asciiData) {
+		return fromAscii(asciiData, 0, asciiData.length);
+	}
+	
+	/**
+	 * Takes a byte array with 7-bit ASCII characters in it, and turns it into a string
+	 * @param asciiData		Data to be processed
+	 * @param offset		Offset in the array
+	 * @param len			Length of data
+	 * @return
+	 */
+	public static String fromAscii(byte[] asciiData, int offset, int len) {
+		return fromBytes(asciiData, offset, len, CHARSET_ASCII);
+	}
+
+	/**
+	 * Turns a string into an UTF-8 series of bytes. 
+	 * @param text				Encoded string
+	 * @param bufferLength		Maximum size of the resulting array
+	 * @return
+	 */
+	public static byte[] toUtf8(String text, int bufferLength) {
+		return toBytes(text, bufferLength, CHARSET_ASCII);
+	}
+	
+	/**
+	 * Takes a byte array with UTF-8 characters in it, and turns it into a string
+	 * @param utfData		Data to be processed
+	 * @return
+	 */
+	public static String fromUtf8(byte[] utfData) {
+		return fromUtf8(utfData, 0, utfData.length);
+	}
+	
+	/**
+	 * Takes a byte array with UTF-8 characters in it, and turns it into a string
+	 * @param utfData		Data to be processed
+	 * @param offset		Offset in the array
+	 * @param len			Length of data
+	 * @return
+	 */
+	public static String fromUtf8(byte[] utfData, int offset, int len) {
+		return fromBytes(utfData, offset, len, CHARSET_UTF8);
 	}
 
 }
